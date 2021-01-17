585b595f7343a210b22c417386f183ce87d93854
Reggie Thompson <reggie.thompson4@gmail.com>
2021-01-17 10:13:53 -0700

    Add turn, opponent properties, add condition to playCard

--- player.js
+++ player.js
@@ -3,27 +3,31 @@ class Player {
     this.id = id;
     this.wins = 0;
     this.hand = [];
+    this.turn = true;
+    this.opponent = checkOpponent(this);
   }
 
   playCard(game) {
-    var cardPlayed = this.hand.splice(0, 1)[0];
-    game.centerPile.unshift(cardPlayed);
+    // var opponent = checkOpponent(this)
+    if(this.turn === true || game[this.opponent].hand.length === 0) {
+      this.turn = false;
+      var cardPlayed = this.hand.splice(0, 1)[0];
+      game.centerPile.unshift(cardPlayed);
+      game[this.opponent].turn = true;
+    } else {
+      console.log('Player cannot play twice unless opponent is out of cards!')
+    }
   }
 
   slapPile(game) {
     var output;
-    var nonSlapper = checkSlapper(this);
+    // var opponent = checkOpponent(this);
     if (isJack() || isDouble() || isSandwich()) {
-      console.log('valid slap')
-      console.log('Jack?', isJack())
-      console.log('Double?', isDouble())
-      console.log('Sandwich?', isSandwich());
       takePile(this, game)
       output = true;
     } else {
-        console.log('invalid slap')
         var topCard = this.hand.shift();
-        game[nonSlapper].hand.push(topCard);
+        game[this.opponent].hand.push(topCard);
         output = false;
     }
 
