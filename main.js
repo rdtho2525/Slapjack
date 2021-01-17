20814e20b48965ff53b2e1209521187d20e23719
Reggie Thompson <reggie.thompson4@gmail.com>
2021-01-16 17:55:33 -0700

    Add takePile and clearPile functions

--- main.js
+++ main.js
@@ -16,19 +16,26 @@ function unhide(element) {
 
 function isJack() {
   if (slapjack.centerPile[0].value === 'jack') {
+    // console.log('Jack?:', slapjack.centerPile[0].value)
     return true
+  } else {
+    return false
   }
 }
 
 function isDouble() {
   if (slapjack.centerPile[0].value === slapjack.centerPile[1].value) {
     return true
+  } else {
+    return false
   }
 }
 
 function isSandwich() {
-  if (slapjack.centerPile[0].value === slapjack.centerPile[2].value) {
+  if (slapjack.centerPile.length > 2 && slapjack.centerPile[0].value === slapjack.centerPile[2].value) {
     return true
+  } else {
+    return false
   }
 }
 
@@ -40,4 +47,14 @@ function checkSlapper(player) {
     nonSlapper = 'playerOne';
   }
   return nonSlapper
+}
+
+function takePile(player, game) {
+  for (var i = 0; i < game.centerPile.length; i++) {
+    player.hand.push(game.centerPile[i])
+  }
+}
+
+function clearPile(game) {
+    game.centerPile = []
 }
